1f41c602d3980b62d1a32e40632163f1a01ac089
derfl8 <alfred.begou@gmail.com>
2025-11-13 12:45:11 +0100

    strange iteration of ft_print

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,38 @@
 /*   By: abegou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 11:25:00 by abegou            #+#    #+#             */
-/*   Updated: 2025/11/13 12:04:10 by abegou           ###   ########.fr       */
+/*   Updated: 2025/11/13 12:40:20 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
+static char	conv_check(const char *conv)
+{
+	size_t	i;
+
+	i = 0;
+	while(*conv != '%')
+		*conv++;
+	*conv++;
+	if (*conv == 'c' || *conv == 's' || *conv == 'p' || *conv == 'd' 
+			|| *conv == 'i' || *conv == 'u' || *conv == 'x' || *conv == 'X' 
+			|| *conv == '%')
+		return (*conv);
+	else
+		return (NULL);
+}
+
 int	ft_printf(const char *conv, char *print)
 {
-	if (conv[0] == '%' && conv[1] == '%' && conv[2] == '\0')
-		return (ft_putchar(conv[1]));
+	char	check;
+
+	check = conv_check(conv);
+	if (!check)
+		return (NULL);
+	if (check  == '%')
+		return (ft_putchar(check));
 	if (conv[0] == '%' && (conv[1] == 'd' || conv[1] == 'i') && conv[2] == '\0')
 		return(ft_putstr(print));
 	(void)print;
